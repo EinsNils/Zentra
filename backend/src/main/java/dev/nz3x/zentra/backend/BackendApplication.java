f2ed053bdb652be34e7a6f8832c026b42052ceeb
Nils Zentgraf <nils.zentgraf@aol.com>
2025-07-15 18:05:00 +0200

    initialize backend project with Spring Boot and Gradle setup

---
+++ backend/src/main/java/dev/nz3x/zentra/backend/BackendApplication.java
@@ -0,0 +1,12 @@
+package dev.nz3x.zentra.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
+}
---
+++ backend/src/test/java/dev/nz3x/zentra/backend/BackendApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.nz3x.zentra.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
